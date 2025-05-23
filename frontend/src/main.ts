cd78b9f309ca5b6c026093c5187aa59478d6e579
Ionut Frisan <ionutfrisan@gmail.com>
2025-05-24 00:57:36 +0300

    auth store

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+
 const app = createApp(App)
 
 app.use(createPinia())
