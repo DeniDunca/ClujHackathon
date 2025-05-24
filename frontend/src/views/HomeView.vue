6d96d6875ccfa7f41c61a5a40c752c26c589caa9
Ionut Frisan <ionutfrisan@gmail.com>
2025-05-24 14:53:48 +0300

    actual homepage but still v0

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,58 +1,22 @@
 <template>
-  <div class="flex gap-4 w-full max-w-3xl m-auto">
-    <Card id="card-prevention">
-      <CardHeader>
-        <CardTitle>
-          <h3 class="font-extrabold text-2xl">
-            {{ t('PREVENTION') }}
-          </h3>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>{{ t('FIND_CANCER_PREVENTION_INFORMATION') }}</p>
-      </CardContent>
-      <CardFooter>
-        <Button>{{ t('LEARN_MORE') }}</Button>
-      </CardFooter>
-    </Card>
-    <Card id="card-chatbot">
-      <CardContent>
-        <div class="flex flex-row gap-2 items-center">
-          <span
-            class="p-2 max-h-12 dark:bg-white/90 bg-black/10 rounded-2xl flex items-center justify-center"
-          >
-            <Bot color="#000" />
-          </span>
-          <span>
-            {{ t('CHATBOT_DESCRIPTION') }}
-          </span>
-        </div>
-      </CardContent>
-      <CardFooter class="w-full">
-        <div class="relative w-full items-center">
-          <Input id="search" type="text" :placeholder="t('ENTER_YOUR_MESSAGE')" class="pr-10 p-6" />
-          <span class="absolute end-2 inset-y-0 flex items-center justify-center px-2">
-            <MoveRight class="size-6 text-muted-foreground" />
-          </span>
-        </div>
-      </CardFooter>
-    </Card>
+<div class="min-h-[calc(100vh-10rem)] flex justify-center flex-col">
+  <div class="flex flex-col gap-4 w-full max-w-2/3 lg:max-w-2xl m-auto items-center text-center">
+    <h2 class="text-blue-500 font-extrabold text-3xl whitespace-pre-line">
+      {{ t('HEALTH_NAVIGATOR') }}
+    </h2>
+    <h3 class="font-semibold text-xl">
+      {{ t('HEALTH_NAVIGATOR_DESCRIPTION') }}
+    </h3>
+    <span class="flex gap-2">
+      <Button >{{ t('LOGIN') }}</Button>
+      <Button variant="outline">{{ t('LEARN_MORE') }}</Button>
+    </span>
   </div>
+</div>
 </template>
 
 <script lang="ts" setup>
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { Bot, MoveRight } from 'lucide-vue-next'
-
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
