29bdef860fb02ac3646c49af32b8c222e15b7bb3
Ionut Frisan <ionutfrisan@gmail.com>
2025-05-25 00:03:04 +0300

    dark mode

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@ import Navbar from '@/components/ui/navbar/Navbar.vue'
 import { Toaster } from '@/components/ui/sonner'
 import 'vue-sonner/style.css'
 
-// const darkMode = useDark()
+const darkMode = useDark()
 import { useAuthStore } from '@/stores/auth.ts'
 
 const authStore = useAuthStore()
