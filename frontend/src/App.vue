7a3ab4cebaadddacc212044a6c17ccfc3ef9e846
Ionut Frisan <ionutfrisan@gmail.com>
2025-05-25 00:02:35 +0300

    new style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-// import { useDark } from '@vueuse/core'
+import { useDark } from '@vueuse/core'
 import Navbar from '@/components/ui/navbar/Navbar.vue'
 import { Toaster } from '@/components/ui/sonner'
 import 'vue-sonner/style.css'
