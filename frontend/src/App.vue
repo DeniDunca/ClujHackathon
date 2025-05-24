cdc55cb351212014b19217cf71b571450a912696
Ionut Frisan <ionutfrisan@gmail.com>
2025-05-24 10:20:36 +0300

    navbar v-

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import {useDark} from "@vueuse/core";
-import {Button} from "@/components/ui/button";
+import { useDark } from '@vueuse/core'
+import { Button } from '@/components/ui/button'
+import Navbar from '@/components/ui/navbar/Navbar.vue'
 
 const darkMode = useDark()
 </script>
 
 <template>
+  <Navbar />
   <div class="flex gap-2">
     <router-link to="/login"> Login </router-link>
     <router-link to="/register"> Register </router-link>
@@ -16,66 +18,4 @@ const darkMode = useDark()
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
---
+++ frontend/src/components/ui/navbar/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="w-screen px-8 py-2 flex flex-row justify-between">
+    <div id="logo" class="font-extrabold text-2xl">Logo</div>
+    <div id="navigation">
+      <Button v-if="!authStore.isAuthenticated">{{ t('LOGIN') }}</Button>
+      <Button v-if="authStore.isAuthenticated">{{ t('LOGOUT') }}</Button>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { useI18n } from 'vue-i18n'
+
+import { useAuthStore } from '@/stores/auth.ts'
+
+const { t } = useI18n()
+
+const authStore = useAuthStore()
+</script>
+
+<style scoped></style>
