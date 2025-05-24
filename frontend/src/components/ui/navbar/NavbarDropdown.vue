bff6c2dfde703597738aa4cd9a5b846b6cff4e79
Ionut Frisan <ionutfrisan@gmail.com>
2025-05-24 11:07:18 +0300

    navbar v0.1

---
+++ frontend/src/components/ui/navbar/NavbarDropdown.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline" size="icon">
+        <User class="w-4 h-4"/>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent class="w-56">
+      <DropdownMenuLabel>{{t('MY_ACCOUNT')}}</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <router-link to="/profile">{{t('PROFILE')}}</router-link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <router-link to="/settings">{{t('SETTINGS')}}</router-link>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>
+        <button @click="logout">{{t('LOGOUT')}}</button>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { useAuthStore } from '@/stores/auth.ts'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { User } from 'lucide-vue-next'
+
+const { t } = useI18n()
+
+const authStore = useAuthStore()
+
+const logout = () => {
+  authStore.logout()
+}
+</script>
